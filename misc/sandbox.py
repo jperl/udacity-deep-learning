5fbf8788d282cf2acf784c5927b89b3de575360b
Jon Perl <perl.jonathan@gmail.com>
2017-03-11 15:25:14 -0500

    Fix weight initialization
    Set stddev=.1

--- misc/sandbox.py
+++ misc/sandbox.py
@@ -1,3 +1,10 @@
-import numpy as np
+# import numpy as np
+# print(np.eye(10))
 
-print(np.eye(10))
+import tensorflow as tf
+
+x = tf.Variable(tf.random_normal([10, 10], stddev=.1))
+
+with tf.Session() as sess:
+   sess.run(tf.global_variables_initializer())
+   print(x.eval())
