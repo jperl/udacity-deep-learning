ad7976980d177101f4e9b66e7fdcbb80f5f8d098
Jon Perl <perl.jonathan@gmail.com>
2017-05-15 00:37:14 -0400

    Tweak hyperparameters add dropout

--- language-translation/command.sh
+++ language-translation/command.sh
@@ -1,3 +1,3 @@
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/Lcw7AbbynX4SVPQWZJ6XtB' --NotebookApp.token='' --NotebookApp.allow_origin='*'
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/CspmZLzyMfizctqifXvaja' --NotebookApp.token='' --NotebookApp.allow_origin='*'
